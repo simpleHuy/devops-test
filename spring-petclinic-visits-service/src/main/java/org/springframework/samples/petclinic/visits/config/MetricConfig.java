af199e0d4bda4d3dfed97009cb8649ed5f7a152c
Hoang Tien Huy <22120134@student.hcmus.edu.vn>
2025-06-13 08:33:50 +0700

    temo

--- spring-petclinic-visits-service/src/main/java/org/springframework/samples/petclinic/visits/config/MetricConfig.java
+++ spring-petclinic-visits-service/src/main/java/org/springframework/samples/petclinic/visits/config/MetricConfig.java
@@ -2,6 +2,13 @@ package org.springframework.samples.petclinic.visits.config;
 
 import io.micrometer.core.aop.TimedAspect;
 import io.micrometer.core.instrument.MeterRegistry;
+import io.micrometer.core.instrument.config.MeterFilter;
+import io.micrometer.core.instrument.Tag;
+import io.micrometer.core.instrument.Tags;
+
+import java.util.Optional;
+
+import org.slf4j.MDC;
 import org.springframework.boot.actuate.autoconfigure.metrics.MeterRegistryCustomizer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -17,6 +24,13 @@ public class MetricConfig {
   @Bean
   TimedAspect timedAspect(MeterRegistry registry) {
     return new TimedAspect(registry);
+  }
+
+  @Bean
+  public MeterFilter traceIdTaggingFilter() {
+    return MeterFilter.commonTags(
+        Tags.of(Tag.of("trace_id", Optional.ofNullable(MDC.get("trace_Id")).orElse("unknown")))
+    );
   }
 
 }

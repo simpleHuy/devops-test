bb22af7d8156bad6cc3d8a164806e54c30dd987b
Hoang Tien Huy <22120134@student.hcmus.edu.vn>
2025-06-13 17:10:18 +0700

    k k

--- spring-petclinic-visits-service/src/main/java/org/springframework/samples/petclinic/visits/config/MetricConfig.java
+++ spring-petclinic-visits-service/src/main/java/org/springframework/samples/petclinic/visits/config/MetricConfig.java
@@ -9,18 +9,11 @@ import io.micrometer.core.instrument.config.MeterFilter;
 import java.util.Optional;
 
 import org.slf4j.MDC;
-import org.springframework.boot.actuate.autoconfigure.metrics.MeterRegistryCustomizer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class MetricConfig {
-
-  @Bean
-  MeterRegistryCustomizer<MeterRegistry> metricsCommonTags() {
-    return registry -> registry.config().commonTags("application", "petclinic");
-  }
-
   @Bean
   TimedAspect timedAspect(MeterRegistry registry) {
     return new TimedAspect(registry);
